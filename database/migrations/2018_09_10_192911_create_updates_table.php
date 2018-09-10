e272b26ee1453939c545068ce3dd1eaae482b8b8
Andrusowski <web@andrus.io>
2018-09-10 21:34:57 +0200

    added update migration

---
+++ database/migrations/2018_09_10_192911_create_updates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('updates', function (Blueprint $table) {
+            $table->string('id', 8);
+            $table->integer('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('updates');
+    }
+}
