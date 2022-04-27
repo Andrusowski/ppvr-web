33c6cd0e11854627b1c168a83660047f517b4c65
Andreas Saks <a.saks@basecom.de>
2022-04-28 00:29:39 +0200

    document api

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,73 @@ use App\Models\Post;
 use App\Models\Rank;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Players
+ *
+ * Get all players (paginated, 50 per page)
+ *
+ * @group Players
+ *
+ * @apiResourceCollection App\Http\Resources\PlayerCollection
+ * @apiResourceModel App\Models\Player paginate=50
+ */
 Route::get('/players', function () {
-    return new PlayerCollection(Player::paginate());
+    return new PlayerCollection(Player::paginate(50));
 });
+/**
+ * Player
+ *
+ * Get a player by id (same as osu player id)
+ *
+ * @group Players
+ *
+ * @apiResource App\Http\Resources\PlayerResource
+ * @apiResourceModel App\Models\Player
+ * @urlParam id integer required player id (same as osu player id) Example: 124493
+ */
 Route::get('/players/{id}', function ($id) {
     return new PlayerResource(Player::findOrFail($id));
 });
 
+/**
+ * Posts
+ *
+ * Get all posts (paginated, 50 per page)
+ *
+ * @group Posts
+ *
+ * @apiResourceCollection App\Http\Resources\PostCollection
+ * @apiResourceModel App\Models\Post paginate=50
+ */
 Route::get('/posts', function () {
-    return new PostCollection(Post::paginate());
+    return new PostCollection(Post::paginate(50));
 });
+/**
+ * Post
+ *
+ * Get a post by id (same as reddit post id)
+ *
+ * @group Posts
+ *
+ * @apiResource App\Http\Resources\PostResource
+ * @apiResourceModel App\Models\Post
+ * @urlParam id string required post id (same as reddit post id) Example: u3rk97
+ */
 Route::get('/posts/{id}', function ($id) {
     return new PostResource(Post::findOrFail($id));
 });
 
+/**
+ * Ranks
+ *
+ * Get the rank history for a player
+ *
+ * @group Ranks
+ *
+ * @apiResourceCollection App\Http\Resources\RankCollection
+ * @apiResourceModel App\Models\Rank
+ * @urlParam playerId integer required player id (same as osu player id) Example: 124493
+ */
 Route::get('/ranks/{playerId}', function ($playerId) {
     return new RankCollection(Rank::wherePlayerId($playerId)->orderByDesc('created_at')->get());
 });
