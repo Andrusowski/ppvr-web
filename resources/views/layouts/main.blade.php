5c97e8990276237f6c6c17c93ffd1974e7c0fd62
Andreas Saks <a.saks@basecom.de>
2022-04-29 01:12:22 +0200

    add ackee

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -110,6 +110,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit-icons.min.js"></script>
 
     <!-- Ackee -->
-    {{--<script async src="https://ackee.andrus.io/ackee.js" data-ackee-server="https://ackee.andrus.io" data-ackee-domain-id="5ecaf1a7-9a5e-4860-9b95-889526ab677c"></script>--}}
+    <script async src="https://ackee.gymir.andrus.io/cooltrack.js" data-ackee-server="https://ackee.gymir.andrus.io" data-ackee-domain-id="3f1bbbb6-ff3f-4090-9c10-79f33667b435"></script>
 </body>
 </html>
